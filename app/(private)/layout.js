9c9d52445080ce1fcff7ff934b19390d2b333ec9
jcbbb <avaz28082000@gmail.com>
2023-07-27 15:08:20 +0500

    fix: highlight active link;

--- app/(private)/layout.js
+++ app/(private)/layout.js
@@ -3,12 +3,14 @@
 import '../globals.css'
 import { Inter } from 'next/font/google'
 import { ApolloWrapper } from "@/lib/apollo-provider";
-import Link from 'next/link';
 import { AuthProvider } from '@/context/auth-context';
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname()
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -18,10 +20,10 @@ export default function RootLayout({ children }) {
               <aside className="bg-white p-4 rounded-md max-w-xs w-full shrink-0 h-[calc(100vh-16rem)]">
                 <ul>
                   <li>
-                    <Link href="/events" className={`block p-2.5 hover:bg-slate-100 duration-200 rounded-md overflow-hidden`}>Events</Link>
+                    <Link href="/events" className={`${pathname === "/events" ? "bg-slate-100" : "hover:bg-slate-100"} block p-2.5 duration-200 rounded-md overflow-hidden`}>Events</Link>
                   </li>
                   <li>
-                    <Link href="/locations" className={`block p-2.5 hover:bg-slate-100 duration-200 rounded-md overflow-hidden`}>Locations</Link>
+                    <Link href="/locations" className={`${pathname === "/locations" ? "bg-slate-100" : "hover:bg-slate-100"} block p-2.5 duration-200 rounded-md overflow-hidden`}>Locations</Link>
                   </li>
                 </ul>
               </aside>
